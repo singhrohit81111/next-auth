fda65eed1221ec17604c5191ee2db031f0f9c631
Rohit Singh <singhrohit8111@gmail.com>
2023-11-02 09:55:19 +0530

    modified users file

--- src/constants/users.ts
+++ src/constants/users.ts
@@ -2,25 +2,21 @@ export const USERS = [
     {
         email: "Ram@gmail.com",
         name: "Ram",
-        password: "1234",
-        designation: "Php Developer",
+        password: "1234"
     },
     {
         email: "Sham@gmail.com",
         name: "Sham",
-        password: "1234",
-        designation: "Angular developer"
+        password: "1234"
     },
     {
         email: "Rohit@gmail.com",
         name: "Rohit",
-        password: "1234",
-        designation: "software developer"
+        password: "1234"
     },
     {
         email: "Raman@gmail.com",
         name: "Raman",
-        password: "1234",
-        designation: " Trainee"
+        password: "1234"
     }
 ]
